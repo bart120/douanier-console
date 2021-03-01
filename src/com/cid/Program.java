48c598d2941aebe0c32771b5e2d0f3af8875343a
Vincent <vleclerc@inow.fr>
2021-03-01 14:19:23 +0100

    menu

--- src/com/cid/Program.java
+++ src/com/cid/Program.java
@@ -1,9 +1,46 @@
 package com.cid;
 
+import java.util.Scanner;
+
 public class Program {
 
+	private static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		while (true) {
+
+			afficherMenu();
+			System.out.println("Votre choix ?");
+			String choix = scan.nextLine();
+
+			switch (choix) {
+			case "1":
+				ajouterContact();
+				break;
+			case "q":
+				scan.close();
+				return;
+
+			default:
+				System.out.println("Saisie inconnue");
+				break;
+			}
+			System.out.println("Appuyez sur Entrée pour continuer.");
+			scan.nextLine();
+		}
+
+	}
+
+	private static void ajouterContact() {
+		
+
+	}
+
+	private static void afficherMenu() {
+		System.out.println("-- MENU --");
+		System.out.println("1- Ajouter un contact");
+		System.out.println("Q- Quitter");
 
 	}
 
